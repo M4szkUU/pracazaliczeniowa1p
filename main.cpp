db161186b4a3001ae931140365b9b0fb2c7ddee1
M4szkUU <90033727+M4szkUU@users.noreply.github.com>
2022-03-20 19:00:18 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    for ( int i=196; i<=216;i++)
+{
+    if((i) % 20==0)
+      {system("PAUSE");system("cls");}
+    cout<< <<SQRTi<<;
+    cout.precision(2);
+}    
+cout <<endl;
+    system("PAUSE");
+    return EXIT_SUCCESS;
+}
